92a6a396cb80ab2cdd5b00e6f8170b2a2e7d3f8f
Dadahon0019 <95474203+dadaxon0019@users.noreply.github.com>
2023-03-14 17:10:48 +0300

    finish sign in with google

--- lib/auth/screens/main_auth_page.dart
+++ lib/auth/screens/main_auth_page.dart
@@ -1,11 +1,38 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:online_market/auth/screens/sign_in_page.dart';
 import 'package:online_market/auth/screens/sign_up_page.dart';
 import 'package:online_market/auth/widgets/big_google_button.dart';
 import 'package:online_market/auth/widgets/sign_button.dart';
 
-class MainAuthPage extends StatelessWidget {
+class MainAuthPage extends StatefulWidget {
   const MainAuthPage({super.key});
+
+  @override
+  State<MainAuthPage> createState() => _MainAuthPageState();
+}
+
+class _MainAuthPageState extends State<MainAuthPage> {
+  Future<UserCredential> signInWithGoogle() async {
+    final navigator = Navigator.of(context);
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    navigator.pushNamedAndRemoveUntil('/home', (Route<dynamic> route) => false);
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,13 +62,14 @@ class MainAuthPage extends StatelessWidget {
             const GoogleButton(
                 nameButton: 'Continue with Facebook',
                 nameImage: 'assets/image/facebook.png'),
-            const GoogleButton(
-                nameButton: 'Continue with Google',
-                nameImage: 'assets/image/google.png'),
             GestureDetector(
-              onTap: () {
-                print('object');
-              },
+              onTap: signInWithGoogle,
+              child: const GoogleButton(
+                  nameButton: 'Continue with Google',
+                  nameImage: 'assets/image/google.png'),
+            ),
+            GestureDetector(
+              onTap: signInWithGoogle,
               child: const GoogleButton(
                   nameButton: 'Continue with Apple',
                   nameImage: 'assets/image/apple-logo.png'),
